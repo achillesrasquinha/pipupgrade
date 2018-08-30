8422a7f0b13a302ec33d1e29dca84638369d6757
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-08-29 23:39:47 -0500

    [FIX] Fix for pip 10

--- pipupgrade/_pip.py
+++ pipupgrade/_pip.py
@@ -1,7 +1,7 @@
 # imports - standard imports
 import pip
 
-PIP9 = int(pip.__version__.split(".")[0]) <= 10
+PIP9 = int(pip.__version__.split(".")[0]) < 9
 
 if PIP9:
     from pip import get_installed_distributions
