b333f10806fcd389c61af04451c4ee6fa842072b
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-08-30 15:05:34 -0500

    [FIX] Fix for pip 10, added tox environments for pip 9, 10, 18

--- pipupgrade/_pip.py
+++ pipupgrade/_pip.py
@@ -1,7 +1,7 @@
 # imports - standard imports
 import pip
 
-PIP9 = int(pip.__version__.split(".")[0]) < 9
+PIP9 = int(pip.__version__.split(".")[0]) < 10
 
 if PIP9:
     from pip import get_installed_distributions
