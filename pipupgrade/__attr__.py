aea44397dc4ae381d992a7cc2a24cdd9d07d7ef2
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-09-01 13:09:44 -0500

    Bumped to Version 0.2.2

--- pipupgrade/__attr__.py
+++ pipupgrade/__attr__.py
@@ -1,5 +1,5 @@
 __name__        = "pipupgrade"
-__version__     = "0.2.1"
+__version__     = "0.2.2"
 __url__         = "https://github.com/achillesrasquinha/pipupgrade"
 __author__      = "Achilles Rasquinha"
 __email__       = "achillesrasquinha@gmail.com"
