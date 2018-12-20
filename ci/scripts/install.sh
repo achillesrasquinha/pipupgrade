78522fbb780c3bcbaaa5620587c15a224cd48fcd
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-12-20 13:35:50 -0600

    ci: Better install script

--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -1,9 +1,11 @@
 export NODE_MAJOR_VERSION="10"
+export SEMANTIC_RELEASE_MAJOR_VERSION="15"
 
-apt-get update                                                                                      \
-    `# Install Dependencies`                                                                        \
-    && apt-get install -y --no-install-recommends                                                   \
-        curl                                                                                        \
-    && curl -sL https://deb.nodesource.com/setup_10.x | bash -                     \
-    && apt-get install -y --no-install-recommends nodejs                                            \
-    && npm install -g @semantic-release@15                                                          \
+apt-get update
+apt-get install -y --no-install-recommends curl
+
+curl -sL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x | bash -
+
+apt-get install -y --no-install-recommends nodejs
+
+npm install -g @semantic-release@$SEMANTIC_RELEASE_MAJOR_VERSION
