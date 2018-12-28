9adbc71217d3eda5780e5b909dbae8fe54679ad3
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-12-28 16:30:40 -0600

    ci: Use bash to execute install.sh

--- ci/scripts/bash/install.sh
+++ ci/scripts/bash/install.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 if [[ $TRAVIS_PYTHON_VERSION == "3.3" ]]; then
+    # https://github.com/aws/base64io-python/issues/4#issuecomment-412696588
     pip install virtualenv==15.2.0
     pip install tox==3.1.3
 fi
 
-export NODE_MAJOR_VERSION="10"
-export SEMANTIC_RELEASE_MAJOR_VERSION="15"
+NODE_MAJOR_VERSION=${NODE_MAJOR_VERSION:="10"}
+SEMANTIC_RELEASE_MAJOR_VERSION=${SEMANTIC_RELEASE_MAJOR_VERSION:="15"}
 
 apt-get update
 apt-get install -y --no-install-recommends curl
