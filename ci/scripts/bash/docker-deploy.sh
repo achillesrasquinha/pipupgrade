884ea9ff6083ff83e9ce5a09d81d5b01eed1d143
Achilles Rasquinha <achillesrasquinha@gmail.com>
2019-01-14 18:40:46 -0600

    ci: Remove DOCKER_HUB_REPONAME

--- ci/scripts/bash/docker-deploy.sh
+++ ci/scripts/bash/docker-deploy.sh
@@ -1,4 +1,4 @@
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-    docker push "$DOCKER_HUB_REPONAME"
+    docker push "$DOCKER_HUB_USERNAME/$PROJECT"
 fi
