ad6af646f2ee6e3037559564f14ede6654e53d5f
achillesrasquinha <achillesrasquinha@gmail.com>
2019-02-06 21:43:01 -0600

    test: Added get_installed_distributions

--- src/pipupgrade/_pip.py
+++ src/pipupgrade/_pip.py
@@ -14,10 +14,11 @@ from pipupgrade.util.environ import value_to_envval
 PIP9 = int(pip.__version__.split(".")[0]) < 10
 
 if PIP9:
+    from pip                 import get_installed_distributions
     from pip.req             import parse_requirements
     from pip.req.req_install import InstallRequirement
-
 else:
+    from pip._internal.utils.misc      import get_installed_distributions
     from pip._internal.req             import parse_requirements
     from pip._internal.req.req_install import InstallRequirement
 
