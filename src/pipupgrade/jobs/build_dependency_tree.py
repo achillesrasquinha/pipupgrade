95f45f588b789be1efd20572ae2f99eb6bf49cf1
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-03-01 15:44:12 -0600

    fix: add releases

--- src/pipupgrade/jobs/build_dependency_tree.py
+++ src/pipupgrade/jobs/build_dependency_tree.py
@@ -56,7 +56,7 @@ def run(*args, **kwargs):
     if osp.exists(path_deptree):
         with gzip.open(path_deptree) as f:
             content = f.read()
-            deptree = json.loads(content)
+            deptree = Dict(json.loads(content))
 
     with make_temp_dir() as dir_path:
         chunk_size  = kwargs.get("chunk_size", 1000)
@@ -92,7 +92,7 @@ def run(*args, **kwargs):
                 if response.ok:
                     data     = response.json()
                     package  = data["info"]["name"]
-                    releases = list(iterkeys(data["releases"]))
+                    releases = list(filter(lambda x: x not in deptree[package], iterkeys(data["releases"])))
 
                     release_chunks = chunkify(releases, 100)
 
