6848a416ce7fb546eb41c188e1a917bd43e9df1b
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-09-16 17:35:07 -0500

    fix: fix for python 2.7

--- src/pipupgrade/__main__.py
+++ src/pipupgrade/__main__.py
@@ -1,5 +1,9 @@
-# imports - module imports
+from __future__ import absolute_import
+
+import sys
+
 from pipupgrade.commands import command as main
 
 if __name__ == "__main__":
-    main()
+    code = main()
+    sys.exit(code)
